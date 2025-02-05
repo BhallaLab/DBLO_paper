2a0d4aa0c4b58390928059aef4f589db56889819
Anal Kumar <analkumar2@gmail.com>
2025-02-06 01:28:57 +0530

    Added active soma + active AIS supp fig

--- SuppFigS1/SuppFigS1.py
+++ SuppFigS1/SuppFigS1.py
@@ -14,14 +14,15 @@ import scikit_posthocs as sp
 # import os
 # import subprocess
 from scipy import signal
-import scipy.stats as scs
+import scipy.stats as sst
+import scipy.signal as ssg
 
 import json
 from pprint import pprint
 
 import pickle
 import scipy
-import MOOSEModel as mm
+import MOOSEModel_ as mm
 from matplotlib.cm import viridis, tab20, tab20c
 from matplotlib.colors import to_rgba
 from copy import deepcopy
@@ -34,13 +35,18 @@ import statsmodels.formula.api as smf
 sns.set(style="ticks")
 sns.set_context("paper")
 
-# fig = plt.figure(figsize=(8, 10))
-fig, axA = plt.subplots(1,1, figsize=(4,4))
+fig = plt.figure(figsize=(8, 3), constrained_layout=False)
+# fig = plt.figure(constrained_layout=True)
+gs = GridSpec(1, 2, figure=fig)
+axA = fig.add_subplot(gs[0, 0])
+axB = fig.add_subplot(gs[0, 1])
+# axC = fig.add_subplot(gs[1, 0])
+# axD = fig.add_subplot(gs[1, 1])
 
 
 # add a, b, c text to each subplot axis
 fig.transFigure.inverted().transform([0.5,0.5])
-for i, ax in enumerate([axA]):
+for i, ax in enumerate([axA, axB]):
     x_infig, y_infig = ax.transAxes.transform([0,1])
     x_infig = x_infig - 20
     y_infig = y_infig + 20
@@ -56,35 +62,98 @@ for i, ax in enumerate([axA]):
         ha="right",
     )
 
-df_expsummaryactiveF = pd.read_pickle("../helperScripts/expsummaryactiveF.pkl")
-df_expactiveF = pd.read_pickle("../helperScripts/expactiveF.pkl")
-
-basemodel_imp_list = []
-file_path = "../Fig9-Implications/activemodels_imp_Eb2_NaTallen.json"
+# Load models from the JSON file
+basemodels_list = []
+file_path = "activemodels_activesoma.json"
 with open(file_path, "r") as file:
-    for line in tqdm(file):
+    for line in file:
         basemodel = json.loads(line)
-        if (basemodel["Features"]["AP1_amp_1.5e-10"]>=df_expsummaryactiveF.loc["AP1_amp_1.5e-10", "10th quantile"]) & (basemodel["Features"]["AP1_amp_1.5e-10"]<=df_expsummaryactiveF.loc["AP1_amp_1.5e-10", "90th quantile"]):
-            basemodel_imp_list.append(basemodel)
+        basemodels_list.append(basemodel)
 
-DBLO_list = np.array([a["Features"]["DBLO_1.5e-10"] for a in basemodel_imp_list])
-AP1thresh_list = np.array([a["Features"]["AP1_thresh_1.5e-10"] for a in basemodel_imp_list])
+DBLO_150pA_list = []
+AP1amp_150pA_list = []
+RA_list = []
+for model in basemodels_list:
+    DBLO_150pA_list.append(model["Features"]["DBLO_1.5e-10"])
+    AP1amp_150pA_list.append(model["Features"]["AP1_amp_1.5e-10"])
+    RA_list.append(model["Parameters"]["Passive"]["sm_RA"])
 
-axA.scatter(DBLO_list*1e3, AP1thresh_list*1e3, s=3, c='C7', label='unified models')
-# axA.scatter(df_expactiveF["DBLO_1.5e-10"]*1e3, df_expactiveF["AP1_thresh_1.5e-10"]*1e3, s=3, c='C0', label='experimental data')
-axA.set_title('DBLO vs AP1 threshold')
-axA.set_ylabel('AP1 Threshold (mV)')
-axA.set_xlabel('DBLO at 150 pA (mV)')
-# axA.legend(frameon=False, loc='lower left')
-axA.text(7, -54, s=f'r = {np.corrcoef(DBLO_list*1e3, AP1thresh_list*1e3)[0][1]:.2f}')
+
+#### Panel A ###################
+# axA.scatter(RA_list, np.array(DBLO_150pA_list)*1e3, c='C7', s=4)
+# axA.set_xlabel(r'RA ($\Omega$m)')
+# axA.set_ylabel('DBLO (mV)')
+# axA.set_xscale('log')
+# m_RAvsDBLO, b_RAvsDBLO, r_RAvsDBLO, pvalue_RAvsDBLO, _ = sst.linregress(RA_list, np.array(DBLO_150pA_list)*1e3)
+
+# axB.scatter(RA_list, np.array(AP1amp_150pA_list)*1e3, c='C7', s=4)
+# axB.set_ylabel(r'Spike height (mV)')
+# axB.set_xlabel(r'RA ($\Omega$m)')
+# axB.set_xscale('log')
+# m_RAvsAP1amp, b_RAvsAP1amp, r_RAvsAP1amp, pvalue_RAvsAP1amp, _ = sst.linregress(RA_list, np.array(AP1amp_150pA_list)*1e3)
+
+df_expsummaryactiveF = pd.read_pickle("../helperScripts/expsummaryactiveF.pkl")
+
+axA.axvspan(14.3, 23.6, color='C2', alpha=0.3)
+# axA.text(20, 100, "High DBLO", fontsize=12, color="black",
+#         horizontalalignment='center', verticalalignment='center', rotation=90)
+axA.axvspan(10, 14.3, color='C8', alpha=0.3)
+# axA.text(14, 100, "Moderate DBLO", fontsize=12, color="black",
+#         horizontalalignment='center', verticalalignment='center', rotation=90)
+axA.axhspan(df_expsummaryactiveF['10th quantile']["AP1_amp_1.5e-10"]*1e3, df_expsummaryactiveF['90th quantile']["AP1_amp_1.5e-10"]*1e3, color='C9', alpha=0.3)
+# axA.text(-10, 120, "Physiological AP1 amplitude", fontsize=12, color="black",
+#         horizontalalignment='center', verticalalignment='center', rotation=0)
+axA.scatter(np.array(DBLO_150pA_list)*1e3, np.array(AP1amp_150pA_list)*1e3, c='C7', s=4)
+axA.set_ylabel(r'Spike height (mV)')
+axA.set_xlabel('DBLO (mV)')
+# axB.set_xscale('log')
+
+
+
+m_DBLOvsAP1amp, b_DBLOvsAP1amp, r_DBLOvsAP1amp, pvalue_DBLOvsAP1amp, _ = sst.linregress(np.array(DBLO_150pA_list)*1e3, np.array(AP1amp_150pA_list)*1e3)
+
+
+#### Panel B #########################
+highRAmodelidx = np.argsort(RA_list)[-1]
+lowRAmodelidx = np.argsort(RA_list)[0]
+highRAmodel = basemodels_list[highRAmodelidx]
+lowRAmodel = basemodels_list[lowRAmodelidx]
+
+t150, Itrace150, Vtrace150, Ca = mm.runModel(highRAmodel, 150e-12, refreshKin=True)
+axB.plot(np.array(t150)*1e3, np.array(Vtrace150)*1e3, label='high RA', c='C2')
+t150, Itrace150, Vtrace150, Ca = mm.runModel(lowRAmodel, 150e-12, refreshKin=True)
+axB.plot(np.array(t150)*1e3, np.array(Vtrace150)*1e3, label='low RA', c='C3')
+axB.set_xlabel("Time (ms)")
+axB.set_ylabel("Voltage (mV)")
+axB.set_title("Representative models")
+axB.legend(frameon=False)
+axB.set_xlim(400, 1300)
+
+# axA.scatter(RA_list[highRAmodelidx], np.array(DBLO_150pA_list)[highRAmodelidx]*1e3, c='C2', s=100, marker='X')
+# axA.scatter(RA_list[lowRAmodelidx], np.array(DBLO_150pA_list)[lowRAmodelidx]*1e3, c='C3', s=100, marker='X')
+# axB.scatter(RA_list[highRAmodelidx], np.array(AP1amp_150pA_list)[highRAmodelidx]*1e3, c='C2', s=100, marker='X')
+# axB.scatter(RA_list[lowRAmodelidx], np.array(AP1amp_150pA_list)[lowRAmodelidx]*1e3, c='C3', s=100, marker='X')
+axA.scatter(np.array(DBLO_150pA_list)[highRAmodelidx]*1e3, np.array(AP1amp_150pA_list)[highRAmodelidx]*1e3, c='C2', s=100, marker='X')
+axA.scatter(np.array(DBLO_150pA_list)[lowRAmodelidx]*1e3, np.array(AP1amp_150pA_list)[lowRAmodelidx]*1e3, c='C3', s=100, marker='X')
+
 
 #####################
 sns.despine(fig=fig)
 plt.tight_layout()
-plt.savefig('SuppFigS1.png', dpi=300)
+plt.savefig('SuppFigS2.png', dpi=300)
 # # plt.savefig('Fig8.pdf', dpi=300)
 plt.show()
 
+
+#####################
+############ Write to NOTES.txt ###############
+with open('NOTES.txt', 'w') as f:
+    # f.write(f'RA vs DBLO linear regressin - {r_RAvsDBLO:1.2f}, pvalue {pvalue_RAvsDBLO:1.2e}\n')
+    # f.write(f'RA vs AP1 amp linear regressin - {r_RAvsAP1amp:1.2f}, pvalue {pvalue_RAvsAP1amp:1.2e}\n')
+    f.write(f'DBLO vs AP1 amp linear regressin - {r_DBLOvsAP1amp:1.2f}, pvalue {pvalue_DBLOvsAP1amp:1.2e}\n')
+    f.write(f"Highest RA model's DBLO - {np.array(DBLO_150pA_list)[highRAmodelidx]*1e3} mV and amp - {np.array(AP1amp_150pA_list)[highRAmodelidx]*1e3} mV and RA - {RA_list[highRAmodelidx]}\n")
+    f.write(f"Lowest RA model's DBLO - {np.array(DBLO_150pA_list)[lowRAmodelidx]*1e3} mV and amp - {np.array(AP1amp_150pA_list)[lowRAmodelidx]*1e3} mV and RA - {RA_list[lowRAmodelidx]}\n")
+    f.write(f'Highest DBLO - {np.max(DBLO_150pA_list)*1e3} mV\n')
 
 
 
